ac9c3d313db79f2fa5c4d77237e6d4181e535061
Bruno Amorim <0brunoamorim0@gmail.com>
2022-02-17 15:44:03 -0300

    DBcreation

--- Back/src/ProjetoEvento.API/Controllers/EventoController.cs
+++ Back/src/ProjetoEvento.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProjetoEvento.API.Data;
 using ProjetoEvento.API.Models;
 
 namespace ProjetoEvento.API.Controllers
@@ -13,41 +14,24 @@ namespace ProjetoEvento.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[]{
-             new Evento(){
-                EventoId =1,
-                Tema = "Angular 11 e .NET5",
-                Local = "Belo Horizonte",
-                Lote = "1ºlote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-             new Evento(){
-                EventoId =2,
-                Tema = "Angular 11 e Novidades",
-                Local = "Londrina",
-                Lote = "2ºlote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto1.png"
-             }
-        };
-        public EventoController()
+        
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
             
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
                 
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id );
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id );
                 
         }    
 
